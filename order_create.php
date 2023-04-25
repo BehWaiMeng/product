d011e7bb3df61e6add2decc6928d2748b19c6a9e
BEH WAI MENG <bbehwaimeng@gmail.com>
2023-04-25 08:21:39 +0800

    createordersuccesssave

--- order_create.php
+++ order_create.php
@@ -6,25 +6,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-
-
 </head>
 
 <body>
     <?php
-
     session_start();
     if (!isset($_SESSION["user"])) {
         $_SESSION["warning"] = "You must be logged in to access this page.";
         header("Location: login.php");
     }
-
-
-
-    include 'navbar.php'; ?>
-
-
+    include 'navbar.php';
+    ?>
 
     <!-- container -->
     <div class="container">
@@ -33,29 +25,30 @@
         </div>
 
         <?php
-
         // include database connection
         include 'config/database.php';
 
-        // Get all categories
-        $query = "SELECT * FROM orders";
+        // Get all product names
+        $query = "SELECT name FROM products";
         try {
             $stmt = $con->prepare($query);
             $stmt->execute();
-            $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $product_names = $stmt->fetchAll(PDO::FETCH_COLUMN);
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
 
         if ($_POST) {
-
-
-
             try {
                 // posted values
                 $order_no = htmlspecialchars(strip_tags($_POST['order_no']));
                 $customer_name = htmlspecialchars(strip_tags($_POST['customer_name']));
-
+                $product1 = htmlspecialchars(strip_tags($_POST['product1']));
+                $product1_quantity = htmlspecialchars(strip_tags($_POST['product1_quantity']));
+                $product2 = htmlspecialchars(strip_tags($_POST['product2']));
+                $product2_quantity = htmlspecialchars(strip_tags($_POST['product2_quantity']));
+                $product3 = htmlspecialchars(strip_tags($_POST['product3']));
+                $product3_quantity = htmlspecialchars(strip_tags($_POST['product3_quantity']));
 
                 //check if any field is empty
                 if (empty($order_no)) {
@@ -65,10 +58,6 @@
                 if (empty($customer_name)) {
                     $customer_name_error = "Please enter customer name";
                 }
-
-
-
-
 
                 //check if there are any errors
                 if (!isset($order_no_error) && !isset($customer_name_error)) {
@@ -84,14 +73,20 @@
                         echo "<div class='alert alert-danger'>The order number already exists. Please enter a unique order number.</div>";
                     } else {
                         // insert query
-                        $query = "INSERT INTO orders SET order_no=:order_no, order_date=:order_date, customer_name=:customer_name";
+                        $query = "INSERT INTO orders SET order_no=:order_no, order_date=:order_date,                     customer_name=:customer_name, product1=:product1, product1_quantity=:product1_quantity, product2=:product2, product2_quantity=:product2_quantity, product3=:product3, product3_quantity=:product3_quantity";
 
                         // prepare query for execution
                         $stmt = $con->prepare($query);
-
                         // bind the parameters
                         $stmt->bindParam(':order_no', $order_no);
                         $stmt->bindParam(':customer_name', $customer_name);
+                        $stmt->bindParam(':product1', $product1);
+                        $stmt->bindParam(':product1_quantity', $product1_quantity);
+                        $stmt->bindParam(':product2', $product2);
+                        $stmt->bindParam(':product2_quantity', $product2_quantity);
+                        $stmt->bindParam(':product3', $product3);
+                        $stmt->bindParam(':product3_quantity', $product3_quantity);
+
                         // specify when this record was inserted to the database
                         $order_date = date('Y-m-d H:i:s');
                         $stmt->bindParam(':order_date', $order_date);
@@ -104,7 +99,7 @@
                         }
                     }
                 } else {
-                    echo "<div class='alert alert-danger'>Please fill up all the empty places.</div>";
+                    echo "<div class='alert alert-danger'>Please fill up all the required fields.</div>";
                 }
             }
             // show error
@@ -114,15 +109,15 @@
         }
         ?>
 
-
-
-
         <!-- html form here where the product information will be entered -->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
             <table class='table table-hover table-responsive table-bordered'>
                 <tr>
                     <td>Order ID</td>
-                    <td><input type="text" name="order_no" class="form-control" value="<?php echo isset($order_no) ? htmlspecialchars($order_no) : ''; ?>" /></td>
+                    <td>
+                        <input type="text" name="order_no" class="form-control" value="<?php echo isset($order_no) ? htmlspecialchars($order_no) : ''; ?>" />
+                        <?php if (isset($order_no_error)) { ?><span class="text-danger"><?php echo $order_no_error; ?></span><?php } ?>
+                    </td>
                 </tr>
                 <tr>
                     <td>Customer Name</td>
@@ -131,18 +126,45 @@
                 </tr>
                 <tr>
                     <td>Product 1</td>
-                    <td><input type="text" name="product1" class="form-control" /></td>
+                    <td>
+                        <select name="product1" class="form-control">
+                            <option value="">-- Select Product --</option>
+                            <?php if ($product_names != null) {
+                                foreach ($product_names as $name) { ?>
+                                    <option value="<?php echo $name; ?>" <?php echo isset($product1) && $product1 == $name ? 'selected' : ''; ?>><?php echo $name; ?></option>
+                            <?php }
+                            } ?>
+                        </select>
+                    </td>
                     <td><input type="number" name="product1_quantity" class="form-control" min="1" /></td>
                 </tr>
                 <tr>
                     <td>Product 2</td>
-                    <td><input type="text" name="product2" class="form-control" /></td>
+                    <td>
+                        <select name="product2" class="form-control">
+                            <option value="">-- Select Product --</option>
+                            <?php if ($product_names != null) {
+                                foreach ($product_names as $name) { ?>
+                                    <option value="<?php echo $name; ?>" <?php echo isset($product2) && $product2 == $name ? 'selected' : ''; ?>><?php echo $name; ?></option>
+                            <?php }
+                            } ?>
+                        </select>
+                    </td>
                     <td><input type="number" name="product2_quantity" class="form-control" min="1" /></td>
                 </tr>
                 <tr>
                     <td>Product 3</td>
-                    <td><input type="text" name="product3" class="form-control" /></td>
-                    <td><input type="number" name="product3_quantity" class="form-control" min="1" /></td>
+                    <td>
+                        <select name="product3" class="form-control">
+                            <option value="">-- Select Product --</option>
+                            <?php if ($product_names != null) {
+                                foreach ($product_names as $name) { ?>
+                                    <option value="<?php echo $name; ?>" <?php echo isset($product3) && $product3 == $name ? 'selected' : ''; ?>><?php echo $name; ?></option>
+                            <?php }
+                            } ?>
+                        </select>
+                    </td>
+                    <td><input type="number" name="product3_quantity" class="form-control" </td>
                 </tr>
                 <tr>
                     <td></td>
@@ -154,8 +176,7 @@
             </table>
         </form>
 
-        <!-- end .container -->
-
+    </div> <!-- end .container -->
 </body>
 
 </html>
