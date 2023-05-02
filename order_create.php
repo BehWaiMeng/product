231c1216363fd4bce8a64e9de48637ab1775e3c3
BEH WAI MENG <bbehwaimeng@gmail.com>
2023-05-02 10:24:23 +0800

    addDeleteOrdercreate

--- order_create.php
+++ order_create.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <title>Create Order</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -19,18 +19,14 @@
     ?>
 
     <!-- container -->
-    <div class="container">
-        <div class="page-header">
+    <div class="container-fluid mt-5 p-5 mb-4">
+        <div class="page-header text-center mb-4">
             <h1>Create Order</h1>
         </div>
 
         <?php
         // include database connection
         include 'config/database.php';
-
-        // Get all product names
-
-
 
         // Get all customer names
         $query = "SELECT username FROM customers";
@@ -42,132 +38,90 @@
             echo "Error: " . $e->getMessage();
         }
 
-        if ($_POST) {
-            try {
-                // posted values
-                $customer_name = htmlspecialchars(strip_tags($_POST['customer_name']));
-                $products = $_POST['product'];
-                $quantities = $_POST['quantity'];
+        // Your PHP code to handle form submission
 
-                if (empty($customer_name)) {
-                    $customer_name_error = "Please enter customer name";
-                }
-                if (empty($products)) {
-                    $products_error = "Please select atleast a product";
-                }
-                if (empty($quantities)) {
-                    $quantities_error = "Please select atleast a quantity";
-                }
-
-                if (!isset($customer_name_error) && !isset($products_error) && !isset($quantities_error)) {
-                    // Insert the order into the database
-                    $query = "INSERT INTO orders SET order_date=:order_date, customer_name=:customer_name";
-                    $stmt = $con->prepare($query);
-                    $stmt->bindParam(':customer_name', $customer_name);
-                    $order_date = date('Y-m-d H:i:s');
-                    $stmt->bindParam(':order_date', $order_date);
-                    //$stmt->execute();
-                    if ($stmt->execute()) {
-                        $order_id = $con->lastInsertId();
-                        for ($x = 0; $x < count($products); $x++) {
-                            // Insert the order details into the database
-                            $query = "INSERT INTO order_details SET order_id=:order_id, product_id=:product_id, quantity=:quantity";
-                            $stmt = $con->prepare($query);
-                            $stmt->bindParam(':order_id', $order_id);
-                            $stmt->bindParam(':product_id', $products[$x]);
-                            $stmt->bindParam(':quantity', $quantities[$x]);
-                            $stmt->execute();
-                        }
-
-
-
-
-                        /*foreach ($products as $index => $product) {
-                            if (!empty($product) && !empty($quantities[$index])) {
-                                $quantity = htmlspecialchars(strip_tags($quantities[$index]));
-                                $stmt->execute();
-                            }
-                        }*/
-
-                        echo "<div class='alert alert-success'>Record was saved.</div>";
-                    } else {
-                        echo "<div class='alert alert-danger'>Unable to save record.</div>";
-                    }
-                } else {
-                    echo "<div class='alert alert-danger'>Please fill up all the required fields.</div>";
-                }
-            }
-            // show error
-            catch (PDOException $exception) {
-                die('ERROR: ' . $exception->getMessage());
-            }
-        }
         ?>
 
         <!-- html form here where the product information will be entered -->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-            <table class='table table-hover table-responsive table-bordered'>
+            <div class="row">
+                <div class="col-12 mb-2 ">
+                    <label class="order-form-label">Username</label>
+                </div>
 
-                <tr>
-                    <td>Select Customer</td>
-                    <td>
-                        <select name="customer_name" class="form-control">
-                            <option value="">-- Select Customer --</option>
-                            <?php if ($customer_names != null) {
-                                foreach ($customer_names as $username) { ?>
-                                    <option value="<?php echo $username; ?>" <?php echo isset($customer_name) && $customer_name == $username ? 'selected' : ''; ?>><?php echo $username; ?></option>
-                            <?php }
-                            } ?>
+                <div class="col-12 mb-2">
+                    <select name="customer_name" class="form-control">
+                        <option value="">-- Select Customer --</option>
+                        <?php if ($customer_names != null) {
+                            foreach ($customer_names as $username) { ?>
+                                <option value="<?php echo $username; ?>"><?php echo $username; ?></option>
+                        <?php }
+                        } ?>
+                    </select>
+                </div>
+            </div>
+
+            <div class="pRow">
+                <div class="row">
+                    <div class="col-8 mb-2 ">
+                        <label class="order-form-label">Product</label>
+                    </div>
+
+                    <div class="col-4 mb-2">
+                        <label class="order-form-label">Quantity</label>
+                    </div>
+
+                    <div class="col-8 ">
+                        <select class="form-select mb-3" name="product[]" aria-label="form-select-lg example">
+                            <option value="" selected>Choose your product </option>
+                            <?php
+                            $query = "SELECT * FROM products";
+                            $stmtproduct = $con->prepare($query);
+                            $stmtproduct->execute();
+
+                            while ($product_row = $stmtproduct->fetch(PDO::FETCH_ASSOC)) {
+                                extract($product_row);
+                                echo "<option value=$id>$name</option>";
+                            }
+                            ?>
                         </select>
-                        <?php if (isset($customer_name_error)) { ?><span class="text-danger"><?php echo $customer_name_error; ?></span><?php } ?>
-                    </td>
-                </tr>
+                    </div>
 
-                <?php for ($i = 1; $i <= 3; $i++) { ?>
-                    <tr>
-                        <td>Product <?php echo $i; ?></td>
-                        <td>
-                            <select name="product[]" class="form-control">
-                                <option value="">-- Select Product --</option>
-                                <?php /*if ($product_names != null) {
-                                    foreach ($product_names as $name && ) { ?>
-                                        <option value="<?php echo $name; */
-                                ?>">
+                    <div class="col-4 ">
+                        <input type="number" name="quantity[]" class="form-control" min="1" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-8 mt-3">
+                <input type="button" value="Add More" class="add_one btn btn-warning" />
+                <input type="button" value="Delete" class="delete_one btn btn-warning" />
+            </div>
 
-                                <?php
-                                $query = "SELECT * FROM products";
-                                $stmtproduct = $con->prepare($query);
-                                $stmtproduct->execute();
-
-                                while ($product_row = $stmtproduct->fetch(PDO::FETCH_ASSOC)) {
-                                    extract($product_row);
-                                    echo "<option value= $id>$name</option>";
-                                }
-
-                                ?>
-
-
-
-
-
-                            </select>
-                        </td>
-                        <td><input type="number" name="quantity[]" class="form-control" min="1" /></td>
-                    <?php } ?>
-                    </tr>
-
-
-                    <tr>
-                        <td></td>
-                        <td>
-                            <input type='submit' value='Save' class='btn btn-primary' />
-                            <a href='order_read.php' class='btn btn-danger'>Back to read orders</a>
-                        </td>
-                    </tr>
-            </table>
+            <div class="row mt-4">
+                <div class="col-12">
+                    <input type='submit' value='Save' class='btn btn-primary' />
+                    <a href='order_read.php' class='btn btn-danger'>Back to read orders</a>
+                </div>
+            </div>
         </form>
-
     </div> <!-- end .container -->
+
+    <script>
+        document.addEventListener('click', function(event) {
+            if (event.target.matches('.add_one')) {
+                var element = document.querySelector('.pRow');
+                var clone = element.cloneNode(true);
+                element.after(clone);
+            }
+            if (event.target.matches('.delete_one')) {
+                var total = document.querySelectorAll('.pRow').length;
+                if (total > 1) {
+                    var element = document.querySelector('.pRow:last-child');
+                    element.remove();
+                }
+            }
+        }, false);
+    </script>
 </body>
 
 </html>
