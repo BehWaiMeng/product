1529f27ec7e2252ee6b865b3872f90afaf3dc78d
BEH WAI MENG <bbehwaimeng@gmail.com>
2023-05-09 07:20:33 +0800

    madecustomerUpdate

---
+++ customer_update.php
@@ -0,0 +1,128 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Update Customer</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    session_start();
+    if (!isset($_SESSION["user"])) {
+        $_SESSION["warning"] = "You must be logged in to access this page.";
+        header("Location: login.php");
+    }
+
+    include 'navbar.php';
+    ?>
+
+    <div class="container">
+        <div class="page-header">
+            <h1>Update Customer</h1>
+        </div>
+
+        <?php
+        // Check if the form was submitted
+        if ($_POST) {
+            // Include database connection
+            include 'config/database.php';
+
+            // Prepare update query
+            $query = "UPDATE customers SET fname=:fname, lname=:lname, gender=:gender, dob=:dob, status=:status, modified=:modified WHERE username=:username";
+            $stmt = $con->prepare($query);
+
+            // Posted values
+            $username = htmlspecialchars(strip_tags($_POST['username']));
+            $fname = htmlspecialchars(strip_tags($_POST['fname']));
+            $lname = htmlspecialchars(strip_tags($_POST['lname']));
+            $gender = htmlspecialchars(strip_tags($_POST['gender']));
+            $dob = htmlspecialchars(strip_tags($_POST['dob']));
+            $status = htmlspecialchars(strip_tags($_POST['status']));
+
+            // Get the current timestamp for the modified column
+            $modified = date('Y-m-d H:i:s');
+
+            // Bind the parameters
+            $stmt->bindParam(':username', $username);
+            $stmt->bindParam(':fname', $fname);
+            $stmt->bindParam(':lname', $lname);
+            $stmt->bindParam(':gender', $gender);
+            $stmt->bindParam(':dob', $dob);
+            $stmt->bindParam(':status', $status);
+            $stmt->bindParam(':modified', $modified);
+
+            // Execute the query
+            if ($stmt->execute()) {
+                echo "<div class='alert alert-success'>Customer details were updated.</div>";
+                header("Location: {$_SERVER['PHP_SELF']}?username=" . urlencode($username));
+                exit();
+            } else {
+                echo "<div class='alert alert-danger'>Unable to update customer details.</div>";
+            }
+        }
+        ?>
+
+        <?php
+        // Get passed parameter value, in this case, the record ID
+        $username2 = isset($_GET['username']) ? $_GET['username'] : die('ERROR: Record Username not found.');
+
+        // Include database connection
+        include 'config/database.php';
+
+        // Read current record's data
+        try {
+            $query = "SELECT * FROM customers WHERE username = ?";
+            $stmt = $con->prepare($query);
+            $stmt->bindParam(1, $username2);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($row !== false) {
+                extract($row);
+            } else {
+                die('ERROR: Could not find customer with the given username.');
+            }
+        } catch (PDOException $exception) {
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+            <input type="hidden" name="username" value="<?php echo htmlspecialchars($username2, ENT_QUOTES); ?>">
+            <div class="form-group">
+                <label>First Name</label>
+                <input type="text" name="fname" value="<?php echo htmlspecialchars($fname, ENT_QUOTES); ?>" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Last Name</label>
+                <input type="text" name="lname" value="<?php echo htmlspecialchars($lname, ENT_QUOTES); ?>" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Gender</label>
+                <select name="gender" class="form-control">
+                    <option value="Male" <?php echo ($gender == "Male") ? "selected" : ""; ?>>Male</option>
+                    <option value="Female" <?php echo ($gender == "Female") ? "selected" : ""; ?>>Female</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Date of Birth</label>
+                <input type="date" name="dob" value="<?php echo htmlspecialchars($dob, ENT_QUOTES); ?>" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Status</label>
+                <select name="status" class="form-control">
+                    <option value="Active" <?php echo ($status == "Active") ? "selected" : ""; ?>>Active</option>
+                    <option value="Inactive" <?php echo ($status == "Inactive") ? "selected" : ""; ?>>Inactive</option>
+                </select>
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="Update">
+            <a href="customer_read.php" class="btn btn-danger">Cancel</a>
+        </form>
+    </div>
+</body>
+
+</html>
--- product_update.php
+++ product_update.php
@@ -3,20 +3,34 @@
 
 <head>
     <title>PDO - Read Records - PHP CRUD Tutorial</title>
-    <!-- Latest compiled and minified Bootstrap CSS →
-   
+    <!-- Latest compiled and minified Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <!-- custom css →
+    <!-- custom css -->
     <style>
-       .m-r-1em{ margin-right:1em; }
-       .m-b-1em{ margin-bottom:1em; }
-       .m-l-1em{ margin-left:1em; }
-       .mt0{ margin-top:0; }
+        .m-r-1em {
+            margin-right: 1em;
+        }
+
+        .m-b-1em {
+            margin-bottom: 1em;
+        }
+
+        .m-l-1em {
+            margin-left: 1em;
+        }
+
+        .mt0 {
+            margin-top: 0;
+        }
     </style>
 </head>
+
 <body>
-    <!-- container →
+    <?php
+
+    include 'navbar.php'; ?>
+    <!-- container -->
     <div class="container">
         <div class="page-header">
             <h1>Update Product</h1>
@@ -56,69 +70,66 @@
         }
         ?>
 
-<?php
-// check if form was submitted
-if ($_POST) {
-    try {
-        // write update query
-        // in this case, it seemed like we have so many fields to pass and
-        // it is better to label them and not use question marks
-        $query = "UPDATE products
+        <?php
+        // check if form was submitted
+        if ($_POST) {
+            try {
+                // write update query
+                // in this case, it seemed like we have so many fields to pass and
+                // it is better to label them and not use question marks
+                $query = "UPDATE products
                   SET name=:name, description=:description,
    price=:price WHERE id = :id";
-        // prepare query for excecution
-        $stmt = $con->prepare($query);
-        // posted values
-        $name = htmlspecialchars(strip_tags($_POST['name']));
-        $description = htmlspecialchars(strip_tags($_POST['description']));
-        $price = htmlspecialchars(strip_tags($_POST['price']));
-        // bind the parameters
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':description', $description);
-        $stmt->bindParam(':price', $price);
-        $stmt->bindParam(':id', $id);
-        // Execute the query
-        if ($stmt->execute()) {
-            echo "<div class='alert alert-success'>Record was updated.</div>";
-        } else {
-            echo "<div class='alert alert-danger'>Unable to update record. Please try again.</div>";
+                // prepare query for excecution
+                $stmt = $con->prepare($query);
+                // posted values
+                $name = htmlspecialchars(strip_tags($_POST['name']));
+                $description = htmlspecialchars(strip_tags($_POST['description']));
+                $price = htmlspecialchars(strip_tags($_POST['price']));
+                // bind the parameters
+                $stmt->bindParam(':name', $name);
+                $stmt->bindParam(':description', $description);
+                $stmt->bindParam(':price', $price);
+                $stmt->bindParam(':id', $id);
+                // Execute the query
+                if ($stmt->execute()) {
+                    echo "<div class='alert alert-success'>Record was updated.</div>";
+                } else {
+                    echo "<div class='alert alert-danger'>Unable to update record. Please try again.</div>";
+                }
+            }
+            // show errors
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
         }
-    }
-    // show errors
-    catch (PDOException $exception) {
-        die('ERROR: ' . $exception->getMessage());
-    }
-} ?>
+        ?>
 
-
-
-       <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post">
-    <table class='table table-hover table-responsive table-bordered'>
-        <tr>
-            <td>Name</td>
-            <td><input type='text' name='name' value="<?php echo htmlspecialchars($name, ENT_QUOTES);  ?>" class='form-control' /></td>
-        </tr>
-        <tr>
-            <td>Description</td>
-            <td><textarea name='description' class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></textarea></td>
-        </tr>
-        <tr>
-            <td>Price</td>
-            <td><input type='text' name='price' value="<?php echo htmlspecialchars($price, ENT_QUOTES);  ?>" class='form-control' /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <input type='submit' value='Save Changes' class='btn btn-primary' />
-                <a href='index.php' class='btn btn-danger'>Back to read products</a>
-            </td>
-        </tr>
-    </table>
-</form>
-
-
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Name</td>
+                    <td><input type='text' name='name' value="<?php echo htmlspecialchars($name, ENT_QUOTES);  ?>" class='form-control' required /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><textarea name='description' class='form-control' required><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></textarea></td>
+                </tr>
+                <tr>
+                    <td>Price</td>
+                    <td><input type='text' name='price' value="<?php echo htmlspecialchars($price, ENT_QUOTES);  ?>" class='form-control' required /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save Changes' class='btn btn-primary' />
+                        <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
     </div>
     <!-- end .container -->
-    </body>
+</body>
 
 </html>
