024a52b6929a10a44075237c44170ecb5309fe96
BEH WAI MENG <bbehwaimeng@gmail.com>
2023-05-16 03:30:07 +0800

    categoryUpdateLayout

--- category_update.php
+++ category_update.php
@@ -3,54 +3,47 @@
 
 <head>
     <title>PDO - Read Records - PHP CRUD Tutorial</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- custom css -->
-    <style>
-        .m-r-1em {
-            margin-right: 1em;
-        }
+    <?php
 
-        .m-b-1em {
-            margin-bottom: 1em;
-        }
+    //check if it login or not
+    session_start();
+    if (!isset($_SESSION["user"])) {
+        $_SESSION["warning"] = "You must be logged in to access this page.";
+        header("Location: login.php");
+    }
+    include 'navbar.php';
+    ?>
 
-        .m-l-1em {
-            margin-left: 1em;
-        }
-
-        .mt0 {
-            margin-top: 0;
-        }
-    </style>
 </head>
 
 <body>
-    <?php
 
-    include 'navbar.php'; ?>
 
-    <!-- container -->
     <div class="container">
         <div class="page-header">
             <h1>Update Category</h1>
         </div>
-        <?php
-        // get passed parameter value, in this case, the record ID
-        // isset() is a PHP function used to verify if a value is there or not
-        $category_id_update = isset($_GET['category_id']) ? $_GET['category_id'] : die('ERROR: Record ID not found.');
 
-        //include database connection
+        <?php
+        // database connection will be here
         include 'config/database.php';
+
+        // get passed parameter value, in this case, the record ID
+        $category_id = isset($_GET['category_id']) ? $_GET['category_id'] : die('ERROR: Record ID not found.');
 
         // read current record's data
         try {
             // prepare select query
-            $query = "SELECT category_id, category_name, description FROM categories WHERE category_id = ? ";
+            $query = "SELECT category_id, category_name, description FROM categories WHERE category_id = ? LIMIT 0,1";
             $stmt = $con->prepare($query);
 
             // this is the first question mark
-            $stmt->bindParam(1, $category_id_update);
+            $stmt->bindParam(1, $category_id);
 
             // execute our query
             $stmt->execute();
@@ -59,31 +52,23 @@
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
             // values to fill up our form
-            $category_id = $row['category_id'];
             $category_name = $row['category_name'];
             $description = $row['description'];
-        }
-
-        // show error
-        catch (PDOException $exception) {
+        } catch (PDOException $exception) {
             die('ERROR: ' . $exception->getMessage());
         }
         ?>
 
+        <!-- check if form was submitted -->
         <?php
-        // check if form was submitted
         if ($_POST) {
             try {
                 // write update query
-                // in this case, it seemed like we have so many fields to pass and
-                // it is better to label them and not use question marks
-                $query = "UPDATE categories
-                SET category_name=:category_name, description=:description
-                WHERE category_id = :category_id";
+                $query = "UPDATE categories SET category_name=:category_name, description=:description WHERE category_id = :category_id";
 
-
-                // prepare query for excecution
+                // prepare query for execution
                 $stmt = $con->prepare($query);
+
                 // posted values
                 $category_id = htmlspecialchars(strip_tags($_POST['category_id']));
                 $category_name = htmlspecialchars(strip_tags($_POST['category_name']));
@@ -93,6 +78,7 @@
                 $stmt->bindParam(':category_name', $category_name);
                 $stmt->bindParam(':description', $description);
                 $stmt->bindParam(':category_id', $category_id);
+
                 // Execute the query
                 if ($stmt->execute()) {
                     echo "<div class='alert alert-success'>Record was updated.</div>";
@@ -107,6 +93,7 @@
         }
         ?>
 
+        <!-- Moved HTML form outside of the if block -->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?category_id={$category_id}"); ?>" method="post">
             <table class='table table-hover table-responsive table-bordered'>
                 <tr>
@@ -118,12 +105,9 @@
                     <td><textarea name='description' class='form-control' required><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></textarea></td>
                 </tr>
                 <tr>
-                    <td>category_id</td>
-                    <td><input type='text' name='category_id' value="<?php echo htmlspecialchars($category_id, ENT_QUOTES);  ?>" class='form-control' required /></td>
-                </tr>
-                <tr>
                     <td></td>
                     <td>
+                        <input type='hidden' name='category_id' value="<?php echo htmlspecialchars($category_id, ENT_QUOTES);  ?>" />
                         <input type='submit' value='Save Changes' class='btn btn-primary' />
                         <a href='category_read.php' class='btn btn-danger'>Back to read products</a>
                     </td>
@@ -132,7 +116,7 @@
         </form>
     </div>
     <!-- end .container -->
+
 </body>
-?>
 
 </html>
