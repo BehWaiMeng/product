379cb503c1534c30dac11ed6f4bd1d426e8c6881
BEH WAI MENG <bbehwaimeng@gmail.com>
2023-05-15 23:59:34 +0800

    contactPage

--- contact.php
+++ contact.php
@@ -3,59 +3,8 @@
 
 <head>
     <title>Contact Us</title>
-    <title>PDO - Read Records - PHP CRUD Tutorial</title>
     <!-- Latest compiled and minified Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <!-- custom css -->
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-        }
-
-        form {
-            width: 400px;
-            margin: 0 auto;
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        h1 {
-            text-align: center;
-            color: #333;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 10px;
-            color: #555;
-        }
-
-        input[type="text"],
-        input[type="email"],
-        textarea {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            margin-bottom: 20px;
-        }
-
-        input[type="submit"] {
-            background-color: #4CAF50;
-            color: #fff;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #3e8e41;
-        }
-    </style>
 </head>
 
 <body>
@@ -67,17 +16,34 @@
         header("Location: login.php");
     }
 
-    include 'navbar.php'; ?>
-    <h1>Contact Us</h1>
-    <form action="process.php" method="POST">
-        <label>Name:</label>
-        <input type="text" name="name" required>
-        <label>Email:</label>
-        <input type="email" name="email" required>
-        <label>Message:</label>
-        <textarea name="message" rows="5" cols="30" required></textarea>
-        <input type="submit" value="Submit">
-    </form>
+    include 'navbar.php';
+    ?>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Contact Us</h1>
+            <form action="process.php" method="POST">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name:</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="mb-3">
+                    <label for="message" class="form-label">Message:</label>
+                    <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+
+        <div class="d-flex justify-content-between align-items-center">
+
+        </div>
+    </div>
+
 </body>
 
 </html>
