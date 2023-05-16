eae0d730cf623767343b7d1d4c9f9bfdd883eb82
BEH WAI MENG <bbehwaimeng@gmail.com>
2023-05-16 10:20:41 +0800

    backtoreadcustomersfor Updatecustomer

--- customer_update.php
+++ customer_update.php
@@ -235,7 +235,7 @@
                 <td></td>
                 <td>
                     <input type='submit' value='Save Changes' class='btn btn-primary' />
-                    <a href='customers_create.php' class='btn btn-danger'>Back to read products</a>
+                    <a href='customers_create.php' class='btn btn-danger'>Back to read customers</a>
                 </td>
             </tr>
         </table>
